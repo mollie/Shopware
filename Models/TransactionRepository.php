89fdffd3e83739a22c5c85d71ae478d73753a620
Dev <dev@kiener.nl>
2018-09-28 15:15:52 +0200

    Cleaned up the Transaction model and repository

--- Models/TransactionRepository.php
+++ Models/TransactionRepository.php
@@ -9,152 +9,45 @@ use MollieShopware\Models\Transaction;
 use MollieShopware\Components\Constants\PaymentStatus;
 use Exception;
 use DateTime;
+use Shopware\Models\Order\Order;
 
 class TransactionRepository extends ModelRepository
 {
+
     /**
-     * Initialize a new Transaction
-     * @todo: update documentation
+     * Create a new transaction for the given order with the given
+     * mollie Order object. This stores the mollie ID with the
+     * order so it can be recovered later.
      *
-     * @param  string $quoteNumber
-     * @param  float  $amount
-     * @param  string $currency
-     * @param  string $token
-     * @param  string $signature
-     * @return Transaction
+     * @param Order $order
+     * @param \Mollie\Api\Resources\Order $mollie_order
      */
-    public function createNew($signature)
+    public function create(Order $order, \Mollie\Api\Resources\Order $mollie_order)
     {
-        $now = new DateTime;
 
-        $transaction = new Transaction;
+        $transaction = new Transaction();
 
-        $session = [];
-        foreach($_SESSION as $namespace=>$namespace_session){
-
-            foreach($namespace_session as $k=>$v){
-
-                if (in_array($k, ['sOrderVariables'])){
-                    continue;
-                }
-                $session[$k] = $v;
-
-            }
-
-        }
-
-
-
-
-        $transaction->setSignature($signature);
-        $transaction->setUserId(0);
-        $transaction->setUserId(0);
-        $transaction->setPaymentId(0);
-        $transaction->setSessionId(0);
-
-        $transaction->setCreatedAt($now);
-        $transaction->setUpdatedAt($now);
-        $transaction->setSerializedSession(json_encode($session, 128));
+        $transaction->setOrderID($order->getId());
+        $transaction->setMollieID($mollie_order->id);
 
         $this->save($transaction);
 
-        return $transaction;
     }
 
     /**
-     * Update the status on a Transaction
+     * Saves a transaction to database
      *
-     * @param  Transaction $transaction
-     * @param  int         $status
-     * @return Transaction
-     */
-    public function updateStatus(Transaction $transaction, $status)
-    {
-        if (PaymentStatus::isPaymentStatus($status)) {
-            $transaction->setStatus($status);
-            return $this->save($transaction);
-        }
-
-        throw new Exception("{$status} is not a valid PaymentStatus");
-    }
-
-    /**
-     * Update the orderId on a Transaction
-     *
-     * @param  Transaction $transaction
-     * @param  int         $orderId
-     * @return Transaction
-     */
-    public function updateOrderNumber(Transaction $transaction, $orderNumber)
-    {
-        $transaction->setOrderNumber($orderNumber);
-        return $this->save($transaction);
-    }
-
-    public function addException(Transaction $transaction, Exception $exception)
-    {
-        $exceptions = $transaction->getExceptions();
-
-        $exceptions[] = [
-            'message' => $exception->getMessage(),
-            'trace' => $exception->getTrace(),
-            'file' => $exception->getFile(),
-            'line' => $exception->getLine(),
-        ];
-
-        $transaction->setExceptions($exceptions);
-        return $this->save($transaction);
-    }
-
-    /**
-     * Get a Transaction by quoteNumber
-     *
-     * @param  string $quoteNumber
-     * @return Transaction
-     */
-    public function getByQuoteNumber($quoteNumber)
-    {
-        return $this->getEntityManager()
-            ->getRepository(Transaction::class)
-            ->findOneBy([ 'quoteNumber' => $quoteNumber ], [ 'createdAt' => 'DESC' ]);
-    }
-
-    public function getByID($id)
-    {
-        return $this->getEntityManager()
-            ->getRepository(Transaction::class)
-            ->findOneBy([ 'id' => $id ], [ 'createdAt' => 'DESC' ]);
-    }
-
-    /**
-     * Get a Transaction by orderNumber
-     *
-     * @param  string $orderNumber
-     * @return Transaction
-     */
-    public function getByOrderNumber($orderNumber)
-    {
-        return $this->getEntityManager()
-            ->getRepository(Transaction::class)
-            ->findOneBy([ 'orderNumber' => $orderNumber ], [ 'createdAt' => 'DESC' ]);
-    }
-
-    /**
-     * Save a Transaction
-
-     * @param  Transaction $transaction
-     * @return Transaction
+     * @param \MollieShopware\Models\Transaction $transaction
+     * @return \MollieShopware\Models\Transaction
      */
     public function save(Transaction $transaction)
     {
-        $now = new DateTime;
 
-        $transaction->setUpdatedAt($now);
-        
         $this->getEntityManager()->persist($transaction);
         $this->getEntityManager()->flush();
 
-
         return $transaction;
+
     }
+
 }
