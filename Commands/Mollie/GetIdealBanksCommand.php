09572d934f5a94d8708c49a480e526ae864dd86d
Reinder van Bochove <reinder@kiener.nl>
2018-10-24 14:51:46 +0200

    Hotfix voor Klarna Pay Later payment method

--- Commands/Mollie/GetIdealBanksCommand.php
+++ Commands/Mollie/GetIdealBanksCommand.php
@@ -1,6 +1,6 @@
 <?php
 
-    // Mollie Shopware Plugin Version: 1.3.2
+	// Mollie Shopware Plugin Version: 1.3.3
 
 namespace MollieShopware\Commands\Mollie;
 
--- Controllers/Frontend/Mollie.php
+++ Controllers/Frontend/Mollie.php
@@ -1,6 +1,6 @@
 <?php
 
-    // Mollie Shopware Plugin Version: 1.3.2
+	// Mollie Shopware Plugin Version: 1.3.3
 
 use MollieShopware\Components\Base\AbstractPaymentController;
     use MollieShopware\Components\Constants\PaymentStatus;
