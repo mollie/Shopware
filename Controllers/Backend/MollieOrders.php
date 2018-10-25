687435cafba2b5ceb8a90e62191580f7487dfeb9
Reinder van Bochove <reinder@kiener.nl>
2018-10-25 12:43:23 +0200

    Inserted new version number

--- Controllers/Backend/MollieOrders.php
+++ Controllers/Backend/MollieOrders.php
@@ -1,6 +1,6 @@
 <?php
 
-	// Mollie Shopware Plugin Version: 1.3.4
+	// Mollie Shopware Plugin Version: 1.3.5
 
 use MollieShopware\Components\Constants\PaymentStatus;
 use MollieShopware\Models\Transaction;
--- Controllers/Frontend/Mollie.php
+++ Controllers/Frontend/Mollie.php
@@ -1,6 +1,6 @@
 <?php
 
-	// Mollie Shopware Plugin Version: 1.3.4
+	// Mollie Shopware Plugin Version: 1.3.5
 
 use MollieShopware\Components\Base\AbstractPaymentController;
     use MollieShopware\Components\Constants\PaymentStatus;
