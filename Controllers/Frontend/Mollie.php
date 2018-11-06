6dc5d8b194dc08134d9e40a0407cf6f062934139
Reinder van Bochove <reinder@kiener.nl>
2018-11-06 17:00:29 +0100

    End of day commiet

--- Controllers/Frontend/Mollie.php
+++ Controllers/Frontend/Mollie.php
@@ -43,7 +43,6 @@ use MollieShopware\Components\Base\AbstractPaymentController;
          */
         public function directAction()
         {
-
             /**
              * @todo: check if basket exists!!
              */
