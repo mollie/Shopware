55920c73ccb04bec95395d2de65605d746ff5ede
Reinder van Bochove <reinder@kiener.nl>
2019-01-04 15:55:37 +0100

    Added new version number

--- Controllers/Frontend/Mollie.php
+++ Controllers/Frontend/Mollie.php
@@ -1,6 +1,6 @@
 <?php
 
-	// Mollie Shopware Plugin Version: 1.3.11
+	// Mollie Shopware Plugin Version: 1.3.12
 
 use MollieShopware\Components\Logger;
 use MollieShopware\Components\Base\AbstractPaymentController;
--- vendor-edited/replace.php
+++ vendor-edited/replace.php
@@ -1,6 +1,6 @@
 <?php
 
-	// Mollie Shopware Plugin Version: 1.3.11
+	// Mollie Shopware Plugin Version: 1.3.12
 
 /*
  *
