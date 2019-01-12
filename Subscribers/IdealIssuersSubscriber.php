4f9c5f763b552ac461c97607d7719b9194ba522f
Reinder van Bochove <reinder@kiener.nl>
2019-01-12 01:29:53 +0100

    Renamed Subscriber (singular) folder to Subscribers (plural)

--- Subscribers/IdealIssuersSubscriber.php
+++ Subscribers/IdealIssuersSubscriber.php
@@ -2,7 +2,7 @@
 
 	// Mollie Shopware Plugin Version: 1.3.12
 
-namespace MollieShopware\Subscriber;
+namespace MollieShopware\Subscribers;
 
 use Enlight\Event\SubscriberInterface;
 use Enlight_Event_EventArgs;
