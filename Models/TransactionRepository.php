4c1d8330cc5f1119c8e31dfbc5bfb9139bf40ffe
Reinder van Bochove <reinder@kiener.nl>
2019-02-13 11:31:23 +0100

    Code refactoring

--- Models/TransactionRepository.php
+++ Models/TransactionRepository.php
@@ -4,12 +4,8 @@
 
 namespace MollieShopware\Models;
 
-use Doctrine\ORM\QueryBuilder;
 use MollieShopware\Components\Logger;
 use Shopware\Components\Model\ModelRepository;
-use MollieShopware\Components\Constants\PaymentStatus;
-use Shopware\Models\Order\Order;
-use Enlight_Components_Session;
 
 class TransactionRepository extends ModelRepository
 {
@@ -17,76 +13,84 @@ class TransactionRepository extends ModelRepository
      * Create a new transaction for the given order with the given
      * mollie Order object. This stores the mollie ID with the
      * order so it can be recovered later.
-     * @param Order|null $order
+     *
+     * @param \Shopware\Models\Order\Order|null $order
      * @param \Mollie\Api\Resources\Order|null $mollieOrder
+     * @param \Mollie\Api\Resources\Payment|null $molliePayment
+     *
+     * @throws \Exception
+     *
      * @return \MollieShopware\Models\Transaction
      */
-    public function create(Order $order = null, \Mollie\Api\Resources\Order $mollieOrder = null, $molliePayment = null)
+    public function create($order = null, $mollieOrder = null, $molliePayment = null)
     {
-        // get new transaction ID
+        $transaction = new Transaction();
         $transactionId = $this->getLastId() + 1;
 
-        // create the transaction
-        $transaction = new Transaction();
-        $transaction->setId($transactionId);
-        $transaction->setTransactionId('mollie_' . $transactionId);
-        $transaction->setSessionId(Enlight_Components_Session::getId());
+        if (!empty($transaction)) {
+            $transaction->setId($transactionId);
+            $transaction->setTransactionId('mollie_' . $transactionId);
+            $transaction->setSessionId(\Enlight_Components_Session::getId());
 
-        // add the order ID if present
-        if ($order) {
-            $transaction->setOrderId($order->getId());
+            if (!empty($order))
+                $transaction->setOrderId($order->getId());
+
+            if (!empty($mollieOrder))
+                $transaction->setMollieId($mollieOrder->id);
+
+            if (!empty($molliePayment))
+                $transaction->setMolliePaymentId($molliePayment->id);
+
+            $this->save($transaction);
         }
-
-        // add the mollie order ID if present
-        if ($transaction) {
-            $transaction->setMollieId($mollieOrder->id);
-        }
-
-        // add the mollie payment ID if present
-        if ($molliePayment) {
-            $transaction->setMolliePaymentId($molliePayment->id);
-        }
-
-        // save the transaction
-        $this->save($transaction);
 
         return $transaction;
-
     }
 
     /**
-     * Saves a transaction to database
+     * Save a transaction to the database
      *
      * @param \MollieShopware\Models\Transaction $transaction
      * @return \MollieShopware\Models\Transaction
+     * @throws \Exception
      */
-    public function save(Transaction $transaction)
+    public function save($transaction)
     {
         try {
             $this->getEntityManager()->persist($transaction);
             $this->getEntityManager()->flush();
         }
-        catch (Exception $ex) {
-            // @todo Handle exception
+        catch (\Exception $ex) {
+            Logger::log(
+                'error',
+                $ex->getMessage(),
+                $ex
+            );
         }
 
         return $transaction;
     }
 
     /**
+     * Get the most recent transaction for an order
+     *
      * @param \Shopware\Models\Order\Order $order
      * @return Transaction
      */
     public function getMostRecentTransactionForOrder($order)
     {
-        return $this->findOneBy([
+        /** @var Transaction $transaction */
+        $transaction = $this->findOneBy([
             'orderId'=> $order->getId()
         ]);
+
+        return $transaction;
     }
 
     /**
-     * Get the last transaction id
+     * Get the last transaction id from the database
      *
+     * @throws \Exception
      * @return int|null
      */
     public function getLastId()
@@ -94,14 +98,18 @@ class TransactionRepository extends ModelRepository
         $id = null;
 
         try {
-            $result = $this->findOneBy([], ['id' => 'DESC']);
+            /** @var Transaction $transaction */
+            $transaction = $this->findOneBy([], ['id' => 'DESC']);
 
-            if (!empty($result))
-                $id = $result->getId();
+            if (!empty($transaction))
+                $id = $transaction->getId();
         }
         catch (\Exception $ex) {
-            // write exception to log
-            Logger::log('error', $ex->getMessage(), $ex);
+            Logger::log(
+                'error',
+                $ex->getMessage(),
+                $ex
+            );
         }
 
         return $id;
