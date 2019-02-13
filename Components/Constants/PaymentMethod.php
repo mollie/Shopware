2490234e389ff833ffe04b4f3705d653d94afbd3
Reinder van Bochove <reinder@kiener.nl>
2019-02-13 13:47:39 +0100

    Added payment method constants

---
+++ Components/Constants/PaymentMethod.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace MollieShopware\Components\Constants;
+
+class PaymentMethod
+{
+    const BANCONTACT = "bancontact";
+    const BANKTRANSFER = "banktransfer";
+    const BELFIUS = "belfius";
+    const BITCOIN = "bitcoin";
+    const CREDITCARD = "creditcard";
+    const DIRECTDEBIT = "directdebit";
+    const EPS = "eps";
+    const GIFTCARD = "giftcard";
+    const GIROPAY = "giropay";
+    const IDEAL = "ideal";
+    const INGHOMEPAY = "inghomepay";
+    const KBC = "kbc";
+    const PAYPAL = "paypal";
+    const PAYSAFECARD = "paysafecard";
+    const PODIUMCADEAUKAART = "podiumcadeaukaart";
+    const SOFORT = "sofort";
+    const KLARNA_PAY_LATER = "klarnapaylater";
+    const KLARNA_SLICE_IT = "klarnasliceit";
+    const P24 = "przelewy24";
+}
