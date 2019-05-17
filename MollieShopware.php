bf84e040d2c8b05a43bb17d6c637756719aa163f
Reinder van Bochove <reinder@kiener.nl>
2019-05-17 21:08:44 +0200

    Changed payment methods retrieval

--- MollieShopware.php
+++ MollieShopware.php
@@ -270,9 +270,8 @@ class MollieShopware extends Plugin
         $mollie = $this->getMollieClient();
 
         // TODO: get methods in the correct locale (de_DE en_US es_ES fr_FR nl_BE fr_BE nl_NL)
-        $methods = $mollie->methods->all([
-            'resource' => 'orders',
-            'includeWallets' => 'applepay'
+        $methods = $mollie->methods->allActive([
+            'resource' => 'orders'
         ]);
 
         $options = [];
