c4226944a68c48f00cad7effd8556f8c3cf182c0
Milan Mooibroek <milan@kiener.nl>
2020-08-19 12:00:42 +0200

    Updated changelog and version.

--- Components/MollieApiFactory.php
+++ Components/MollieApiFactory.php
@@ -51,7 +51,7 @@ class MollieApiFactory
 
                 // add plugin name and version
                 $this->apiClient->addVersionString(
-                    'MollieShopware/1.5.19'
+                    'MollieShopware/1.5.20'
                 );
             }
             catch (\Exception $ex) {
