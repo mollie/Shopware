1e976ee4c225787a0a80a011f219b97c93022fb9
Christian <cd@dasistweb.de>
2020-10-28 09:41:44 +0100

    NTR: New Release 1.6.6 (#81)

--- MollieShopware.php
+++ MollieShopware.php
@@ -30,7 +30,7 @@ use Shopware\Components\Plugin\Context\UninstallContext;
 class MollieShopware extends Plugin
 {
 
-    const PLUGIN_VERSION = '1.6.5';
+    const PLUGIN_VERSION = '1.6.6';
 
 
     /**
