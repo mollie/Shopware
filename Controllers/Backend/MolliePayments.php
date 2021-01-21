83912a7859e21adad81871c771176ed834b2eeb1
Steffen Beisenherz <s.beisenherz@kellerkinder.de>
2021-01-21 17:34:08 +0100

    [DIWMOLLIE-8] Get Payment Service from the DI

--- Controllers/Backend/MolliePayments.php
+++ Controllers/Backend/MolliePayments.php
@@ -17,7 +17,7 @@ class Shopware_Controllers_Backend_MolliePayments extends Shopware_Controllers_B
     {
         try {
             /** @var PaymentMethodService $paymentMethodService */
-            $paymentMethodService = $this->getPaymentMethodService();
+            $paymentMethodService = $this->container->get('mollie_shopware.payment_method_service');
 
             if ($paymentMethodService !== null) {
                 // Deactivate all Mollie payment methods
@@ -38,91 +38,11 @@ class Shopware_Controllers_Backend_MolliePayments extends Shopware_Controllers_B
 
             Shopware()->Template()->assign('response', 'Success!');
         } catch (\Throwable $e) {
-            $this->logger->addError($e->getMessage());
+            $this->getPluginLogger()->addError($e->getMessage());
 
             $this->response->setStatusCode(500);
             $this->View()->assign('response', $e->getMessage());
         }
-    }
-
-    private function getPaymentMethodService()
-    {
-        /** @var ModelManager $modelManager */
-        $modelManager = $this->container->get('models');
-
-        /** @var MollieApiClient $mollieApiClient */
-        $mollieApiClient = $this->getMollieApiClient();
-
-        /** @var PaymentInstaller $paymentInstaller */
-        $paymentInstaller = $this->container->get('shopware.plugin_payment_installer');
-
-        /** @var PaymentMethodService $paymentMethodService */
-        $paymentMethodService = null;
-
-        /** @var Enlight_Template_Manager $templateManager */
-        $templateManager = $this->container->get('template');
-
-        // Create an instance of the payment method service
-        if (
-            $modelManager !== null
-            && $mollieApiClient !== null
-            && $paymentInstaller !== null
-            && $templateManager !== null
-        ) {
-            $paymentMethodService = new PaymentMethodService(
-                $modelManager,
-                $mollieApiClient,
-                $paymentInstaller,
-                $templateManager,
-                $this->getPluginLogger()
-            );
-        }
-
-        return $paymentMethodService;
-    }
-
-    /**
-     * Returns an instance of the Mollie API client.
-     *
-     * @return MollieApiClient
-     */
-    protected function getMollieApiClient()
-    {
-        /** @var Config $config */
-        $config = null;
-
-        /** @var ConfigReader $configReader */
-        $configReader = $this->container->get('shopware.plugin.cached_config_reader');
-
-        /** @var ShopService $shopService */
-        $shopService = new ShopService($this->container->get('models'));
-
-        /** @var MollieApiFactory $factory */
-        $factory = null;
-
-        /** @var MollieApiClient $mollieApiClient */
-        $mollieApiClient = null;
-
-        // Get the config
-        if ($configReader !== null) {
-            $config = new Config($configReader, $shopService);
-        }
-
-        // Get the Mollie API factory service
-        if ($config !== null) {
-            $factory = new MollieApiFactory($config, $this->getPluginLogger());
-        }
-
-        // Create the Mollie API client
-        if ($factory !== null) {
-            try {
-                $mollieApiClient = $factory->create();
-            } catch (Exception $e) {
-                //
-            }
-        }
-
-        return $mollieApiClient;
     }
 
     /**
