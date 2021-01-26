89c2e77b219d4efed15db935dfc61d3a64627987
Thilo Lindner <thilo.lindner@dasistweb.de>
2021-01-26 16:13:16 +0100

    Revert "Revert "MOL-76: Shipping Cost Improvement (#102)""
    This reverts commit 71170dd5c637245acb8a6225ae43c1655b271ebc.

--- Components/Shipping/Providers/CartShippingCostsProvider.php
+++ Components/Shipping/Providers/CartShippingCostsProvider.php
@@ -2,18 +2,32 @@
 
 namespace MollieShopware\Components\Shipping\Providers;
 
-use MollieShopware\Components\Shipping\ShippingCostsProviderInterface;
+use MollieShopware\Components\Services\OrderService;
 use MollieShopware\Components\Shipping\Models\ShippingCosts;
+use MollieShopware\Components\Shipping\ShippingCostsProviderInterface;
+use MollieShopware\Exceptions\OrderNotFoundBySessionIdException;
+use Shopware\Models\Order\Order;
 
 class CartShippingCostsProvider implements ShippingCostsProviderInterface
 {
+    /** @var OrderService */
+    private $orderService;
+
+    /**
+     * @param OrderService $orderService
+     */
+    public function __construct(OrderService $orderService)
+    {
+        $this->orderService = $orderService;
+    }
 
     /**
      * @return ShippingCosts
+     * @throws OrderNotFoundBySessionIdException
      */
     public function getShippingCosts()
     {
-        $shippingCosts = Shopware()->Modules()->Admin()->sGetPremiumShippingcosts();
+        $shippingCosts = $this->getInvoiceShippingCosts();
 
         $unitPrice = 0;
         $unitPriceNet = 0;
@@ -30,6 +44,52 @@ class CartShippingCostsProvider implements ShippingCostsProviderInterface
             $unitPriceNet,
             $taxRate
         );
+    }
+
+    /**
+     * @return array|null
+     * @throws OrderNotFoundBySessionIdException
+     */
+    private function getInvoiceShippingCosts()
+    {
+        $sessionId = Shopware()->Session()->offsetGet('sessionId');
+
+        /** @var Order $order */
+        $order = $this->orderService->getOrderBySessionId($sessionId);
+
+        if ($order === null) {
+            throw new OrderNotFoundBySessionIdException($sessionId);
+        }
+
+        $brutto = round($order->getInvoiceShipping(), 2);
+        $netto = round($order->getInvoiceShippingNet(), 2);
+        $taxRate = $this->getTaxRate($order);
+
+        return [
+            'brutto' => $brutto,
+            'netto' => $netto,
+            'tax' => $taxRate
+        ];
+    }
+
+    /**
+     * @param Order $order
+     * @return float
+     */
+    private function getTaxRate(Order $order)
+    {
+        $taxRate = $order->getInvoiceShippingTaxRate();
+
+        if ($taxRate !== null) {
+            return $taxRate;
+        }
+
+        if ($order->getInvoiceShipping() === $order->getInvoiceShippingNet()) {
+            return 0.0;
+        }
+
+        // taxRate = (unroundedBrutto / unroundedNetto - 1) * 100, eg. (1.19 / 1 - 1) * 100 = 19.0
+        return (round($order->getInvoiceShipping() / $order->getInvoiceShippingNet(), 2) - 1) * 100;
     }
 
 }
