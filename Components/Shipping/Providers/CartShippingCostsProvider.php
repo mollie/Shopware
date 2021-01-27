d414cc3517e1a86df74932b8d6a24c8d2d46eb9e
Thilo <40621547+ThLind@users.noreply.github.com>
2021-01-27 09:46:51 +0100

    MOL-129: fallback shipping costs for direct payments (#119)

--- Components/Shipping/Providers/CartShippingCostsProvider.php
+++ Components/Shipping/Providers/CartShippingCostsProvider.php
@@ -2,18 +2,40 @@
 
 namespace MollieShopware\Components\Shipping\Providers;
 
-use MollieShopware\Components\Shipping\ShippingCostsProviderInterface;
+use MollieShopware\Components\Services\OrderService;
 use MollieShopware\Components\Shipping\Models\ShippingCosts;
+use MollieShopware\Components\Shipping\ShippingCostsProviderInterface;
+use MollieShopware\Exceptions\OrderNotFoundBySessionIdException;
+use Shopware\Models\Order\Order;
 
 class CartShippingCostsProvider implements ShippingCostsProviderInterface
 {
+    /** @var OrderService */
+    private $orderService;
+
+    /**
+     * @param OrderService $orderService
+     */
+    public function __construct(OrderService $orderService)
+    {
+        $this->orderService = $orderService;
+    }
 
     /**
      * @return ShippingCosts
+     * @throws OrderNotFoundBySessionIdException
      */
     public function getShippingCosts()
     {
-        $shippingCosts = Shopware()->Modules()->Admin()->sGetPremiumShippingcosts();
+        try {
+            // Use the already in order calculated shipping costs if they exist
+            $shippingCosts = $this->getInvoiceShippingCosts();
+        } catch (OrderNotFoundBySessionIdException $ex) {
+            // If order not found by session ID use backend shipping costs configuration.
+            // This is an important Fallback for Direct Payment Methods which don't create
+            // orders in database before they redirect to the external direct checkout
+            $shippingCosts = Shopware()->Modules()->Admin()->sGetPremiumShippingcosts();
+        }
 
         $unitPrice = 0;
         $unitPriceNet = 0;
@@ -30,6 +52,52 @@ class CartShippingCostsProvider implements ShippingCostsProviderInterface
             $unitPriceNet,
             $taxRate
         );
+    }
+
+    /**
+     * @return array|null
+     * @throws OrderNotFoundBySessionIdException
+     */
+    private function getInvoiceShippingCosts()
+    {
+        $sessionId = Shopware()->Session()->offsetGet('sessionId');
+
+        /** @var Order $order */
+        $order = $this->orderService->getOrderBySessionId($sessionId);
+
+        if ($order === null) {
+            throw new OrderNotFoundBySessionIdException($sessionId);
+        }
+
+        $brutto = round($order->getInvoiceShipping(), 2);
+        $netto = round($order->getInvoiceShippingNet(), 2);
+        $taxRate = $this->getTaxRate($order);
+
+        return [
+            'brutto' => $brutto,
+            'netto' => $netto,
+            'tax' => $taxRate
+        ];
+    }
+
+    /**
+     * @param Order $order
+     * @return float
+     */
+    private function getTaxRate(Order $order)
+    {
+        $taxRate = $order->getInvoiceShippingTaxRate();
+
+        if ($taxRate !== null) {
+            return $taxRate;
+        }
+
+        if ($order->getInvoiceShipping() === $order->getInvoiceShippingNet()) {
+            return 0.0;
+        }
+
+        // taxRate = (unroundedBrutto / unroundedNetto - 1) * 100, eg. (1.19 / 1 - 1) * 100 = 19.0
+        return (round($order->getInvoiceShipping() / $order->getInvoiceShippingNet(), 2) - 1) * 100;
     }
 
 }
