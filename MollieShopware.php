46eb13455e04e8b0fde3e1100ec35e69d18c1b82
Christian <boxblinkracer@gmx.net>
2021-02-04 08:49:03 +0100

    NTR: Release 1.7.2 (#136)

--- MollieShopware.php
+++ MollieShopware.php
@@ -31,7 +31,7 @@ use Shopware\Components\Plugin\Context\UpdateContext;
 class MollieShopware extends Plugin
 {
 
-    const PLUGIN_VERSION = '1.7.1';
+    const PLUGIN_VERSION = '1.7.2';
 
     const PAYMENT_PREFIX = 'mollie_';
 
