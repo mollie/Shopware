4ac8f0f9104ee875000364b9e1bf0832b9d0c58c
Christian <cd@dasistweb.de>
2021-02-16 11:08:49 +0100

    MOL-168: Ensure correct payment method action after installation (#145)

---
+++ Tests/Components/Installer/PaymentMethodServiceTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace MollieShopware\Tests\Components\Installer;
+
+use MollieShopware\Components\Services\PaymentMethodService;
+use PHPUnit\Framework\TestCase;
+
+class PaymentMethodServiceTest extends TestCase
+{
+
+    /**
+     * This test verifies that our important value for the
+     * payment action is not touched without recognizing it.
+     * It's important to tell Shopware to start a certain action.
+     * That action is always the same for Mollie payments and
+     * must always exist.
+     *
+     * @covers \MollieShopware\Components\Services\PaymentMethodService::installPaymentMethods
+     * @covers \MollieShopware\Components\Services\PaymentMethodService::uninstallPaymentMethods
+     */
+    public function testMollieActionKey()
+    {
+        $this->assertEquals('frontend/Mollie', PaymentMethodService::MOLLIE_ACTION_KEY);
+    }
+
+}
