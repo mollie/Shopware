c885d774bc6dac5532125f8481a21b8ed32e2893
Stephan Altmann <s.altmann@kellerkinder.de>
2021-02-16 15:19:59 +0100

    [DIWMOLLIE-12] add js files

--- MollieShopware.php
+++ MollieShopware.php
@@ -136,6 +136,8 @@ class MollieShopware extends Plugin
         if ($request->getActionName() == 'load') {
             $view->extendsTemplate('backend/mollie_extend_order/view/list/list.js');
             $view->extendsTemplate('backend/mollie_extend_order/controller/list.js');
+            $view->extendsTemplate('backend/mollie_extend_order/model/order_history.js');
+            $view->extendsTemplate('backend/mollie_extend_order/view/detail/order-history.js');
             $view->extendsTemplate('backend/mollie_extend_order_detail/view/detail/position.js');
         }
     }
