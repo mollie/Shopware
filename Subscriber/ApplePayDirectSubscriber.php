fc8f39aa5c6db153b1b7d163d851bc396d60a654
Stephan Altmann <s.altmann@kellerkinder.de>
2021-03-26 17:40:00 +0100

    [DIWMOLLIE-23] remove conditional construct arg

--- Subscriber/ApplePayDirectSubscriber.php
+++ Subscriber/ApplePayDirectSubscriber.php
@@ -32,31 +32,6 @@ use Throwable;
 
 class ApplePayDirectSubscriber implements SubscriberInterface
 {
-    /**
-     * @var null|\Shopware_Components_Modules
-     */
-    private $modules = null;
-
-    /**
-     * @var Account $accountService
-     */
-    private $accountService;
-
-    /**
-     * @var ApplePayPaymentMethod
-     */
-    private $paymentMethodService;
-
-    /**
-     * @param Shopware_Components_Modules|null $modules
-     */
-    public function __construct(Account $accountService, ApplePayPaymentMethod $paymentMethodService, $modules = null)
-    {
-        $this->accountService = $accountService;
-        $this->paymentMethodService = $paymentMethodService;
-        $this->modules = $modules;
-    }
-
     public static function getSubscribedEvents()
     {
         return [
@@ -65,75 +40,7 @@ class ApplePayDirectSubscriber implements SubscriberInterface
             'Enlight_Controller_Action_PostDispatch_Frontend' => 'onFrontendPostDispatch',
             'Enlight_Controller_Action_PostDispatch_Widget' => 'onFrontendPostDispatch',
             'Enlight_Controller_Action_PostDispatch_Frontend_Checkout' => 'onFrontendCheckoutPostDispatch',
-            'Enlight_Controller_Action_PreDispatch_Frontend_Checkout' => 'onFrontendCheckoutPreDispatch',
-            'Enlight_Controller_Action_PostDispatch_Frontend_Account' => 'onFrontendAccountPostDispatch',
         ];
-    }
-
-    /**
-     * @param Enlight_Event_EventArgs $args
-     */
-    public function onFrontendAccountPostDispatch(\Enlight_Event_EventArgs $args)
-    {
-        /** @var Enlight_Controller_Request_RequestHttp $request */
-        $request = $args->get('request');
-
-        if ($request->getActionName() === 'payment') {
-            $view = $args->getSubject()->View();
-            $paymentMeans = $this->removeApplePaymentMethodsFromPaymentMeans($view->sPaymentMeans);
-            $view->sPaymentMeans = $paymentMeans;
-        }
-
-        if ($request->getActionName() !== 'login') {
-            return;
-        }
-
-        if ($this->modules->Admin()->sCheckUser() === false) {
-            return;
-        }
-
-        $currentPaymentMethod = $this->modules->Admin()->sGetUserData()['additional']['payment']['name'];
-
-        try {
-            if ($this->paymentMethodService->isApplePayPaymentMethod($currentPaymentMethod) === false) {
-                return;
-            }
-
-            $userId = (int)$this->modules->Admin()->sGetUserData()['additional']['user']['id'];
-            $paymentId = $this->accountService->getCustomerDefaultNonApplePayPaymentMethod($userId);
-
-            $this->accountService->updateCustomerDefaultPaymentMethod($userId, $paymentId);
-        } catch (Throwable $e) {
-            // prevent login to break if something goes wrong here
-        }
-    }
-
-    /**
-     * @param Enlight_Event_EventArgs $args
-     */
-    public function onFrontendCheckoutPreDispatch(\Enlight_Event_EventArgs $args)
-    {
-        /** @var Enlight_Controller_Request_RequestHttp $request */
-        $request = $args->get('request');
-
-        if ($request->getActionName() !== 'finish') {
-            return;
-        }
-
-        $currentPaymentMethod = $this->modules->Admin()->sGetUserData()['additional']['payment']['name'];
-
-        try {
-            if ($this->paymentMethodService->isApplePayPaymentMethod($currentPaymentMethod) === false) {
-                return;
-            }
-
-            $userId = (int)$this->modules->Admin()->sGetUserData()['additional']['user']['id'];
-            $paymentId = $this->accountService->getCustomerDefaultNonApplePayPaymentMethod($userId);
-
-            $this->accountService->updateCustomerDefaultPaymentMethod($userId, $paymentId);
-        } catch (Throwable $e) {
-            // prevent checkout finish to break if something goes wrong here
-        }
     }
 
     /**
