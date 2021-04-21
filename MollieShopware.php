b64616b1b1824d47ad7f1e4d3831b6edf83e340c
boxblinkracer <boxblinkracer@gmx.net>
2021-04-21 11:01:20 +0200

    Release 1.8.0

--- MollieShopware.php
+++ MollieShopware.php
@@ -32,7 +32,7 @@ use Shopware\Components\Routing\Context;
 
 class MollieShopware extends Plugin
 {
-    const PLUGIN_VERSION = '1.7.5';
+    const PLUGIN_VERSION = '1.8.0';
 
     const PAYMENT_PREFIX = 'mollie_';
 
