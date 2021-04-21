e61349389db31054dea708f33a544c36a2d98ed0
Christian <boxblinkracer@gmx.net>
2021-04-21 08:42:18 +0200

    MOL-247: Maximum Session Token length (#182)

---
+++ Tests/PHPUnit/Components/SessionManager/Services/TokenGenerator/TokenGeneratorTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace MollieShopware\Tests\Components\SessionManager\Services\TokenGenerator;
+
+use MollieShopware\Components\SessionManager\Services\TokenGenerator\TokenGenerator;
+use PHPUnit\Framework\TestCase;
+
+class TokenGeneratorTest extends TestCase
+{
+
+    /**
+     * This test verifies that token length.
+     * This has a maximum length of 25 in order to still see it fully
+     * in the Mollie Dashboard. Otherwise it is displayed with "..." at the end.
+     *
+     * @covers       \MollieShopware\Components\SessionManager\Services\TokenGenerator\TokenGenerator::generateToken
+     */
+    public function testTokenLength()
+    {
+        $generator = new TokenGenerator();
+
+        $token = $generator->generateToken();
+
+        $this->assertEquals(25, strlen($token));
+    }
+
+}
