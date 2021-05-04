cdbb93020828203e504a2ce3d7787e2348c25d63
boxblinkracer <boxblinkracer@gmx.net>
2021-05-04 14:07:53 +0200

    test

--- Tests/Cypress/cypress/integration/storefront/payments/applepay-direct.spec.js
+++
@@ -1,9 +0,0 @@
-describe('Apple Pay Direct', () => {
-
-    it('Domain Verification file has been downloaded', () => {
-
-        cy.request('/.well-known/apple-developer-merchantid-domain-association');
-
-    })
-
-})
