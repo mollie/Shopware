3b74170d1ac2798fe6c6ced08cb2f8d58e9c81f1
boxblinkracer <boxblinkracer@gmx.net>
2021-05-20 09:32:42 +0200

    NTR: Release 1.8.3

--- MollieShopware.php
+++ MollieShopware.php
@@ -32,7 +32,7 @@ use Shopware\Components\Routing\Context;
 
 class MollieShopware extends Plugin
 {
-    const PLUGIN_VERSION = '1.8.2';
+    const PLUGIN_VERSION = '1.8.3';
 
     const PAYMENT_PREFIX = 'mollie_';
 
