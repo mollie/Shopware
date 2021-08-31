c8b160915e1aa8dc44bdcf7f2d53671ba2b85b26
boxblinkracer <boxblinkracer@gmx.net>
2021-08-31 10:56:30 +0200

    NTR: add total cart amount tests to Cypress

--- Tests/Cypress/cypress/integration/storefront/checkout/checkout-full.spec.js
+++ Tests/Cypress/cypress/integration/storefront/checkout/checkout-full.spec.js
@@ -84,12 +84,29 @@ configs.forEach(config => {
 
                     checkout.switchPaymentMethod(payment.name);
 
+                    // grab the total sum of our order from the confirm page.
+                    // we also want to test what the user has to pay in Mollie.
+                    // this has to match!
+                    checkout.getTotalFromConfirm().then(total => {
+                        cy.log("Cart Total: " + total);
+                        cy.wrap(total.toString().trim()).as('totalSum')
+                    });
+
                     checkout.placeOrderOnConfirm();
 
                     // verify that we are on the mollie payment screen
                     // and that our payment method is also visible somewhere in that url
                     cy.url().should('include', 'https://www.mollie.com/paymentscreen/');
                     cy.url().should('include', payment.key);
+
+                    // verify that the price is really the one
+                    // that was displayed in Shopware
+                    cy.get('.header__amount').then(($headerAmount) => {
+                        cy.get('@totalSum').then(totalSum => {
+                            expect($headerAmount.text()).to.contain(totalSum);
+                        });
+                    })
+
 
                     molliePayment.initSandboxCookie();
 
