bd1ae3149e4a16c092ab08bf59da4e0a8b6958e6
Christian <boxblinkracer@gmx.net>
2022-01-12 11:33:48 +0100

    MOL-689: Update Mollie API Client to v2.39 (#309)

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,41 +1,42 @@
-<?php return array(
-    'root' => array(
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
-        'type' => 'library',
-        'install_path' => __DIR__ . '/../../',
-        'aliases' => array(),
-        'reference' => '7c5373d8cd48f45f031bc23dc6fc1b9c1e73199a',
-        'name' => 'mollie/shopware',
-        'dev' => false,
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
     ),
-    'versions' => array(
-        'composer/ca-bundle' => array(
-            'pretty_version' => '1.2.10',
-            'version' => '1.2.10.0',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/./ca-bundle',
-            'aliases' => array(),
-            'reference' => '9fdb22c2e97a614657716178093cd1da90a64aa8',
-            'dev_requirement' => false,
-        ),
-        'mollie/mollie-api-php' => array(
-            'pretty_version' => 'v2.37.1',
-            'version' => '2.37.1.0',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../mollie/mollie-api-php',
-            'aliases' => array(),
-            'reference' => '260adf68db65edd82b6c11f8fa58622ff72b717c',
-            'dev_requirement' => false,
-        ),
-        'mollie/shopware' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../../',
-            'aliases' => array(),
-            'reference' => '7c5373d8cd48f45f031bc23dc6fc1b9c1e73199a',
-            'dev_requirement' => false,
-        ),
+    'reference' => '02b5e058bcb210cfc2dcc2fc1546b6e9d6716be8',
+    'name' => 'mollie/shopware',
+  ),
+  'versions' => 
+  array (
+    'composer/ca-bundle' => 
+    array (
+      'pretty_version' => '1.3.1',
+      'version' => '1.3.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4c679186f2aca4ab6a0f1b0b9cf9252decb44d0b',
     ),
+    'mollie/mollie-api-php' => 
+    array (
+      'pretty_version' => 'v2.39.0',
+      'version' => '2.39.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a47d4449973c83e918db39f860b5e9076aeb367d',
+    ),
+    'mollie/shopware' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '02b5e058bcb210cfc2dcc2fc1546b6e9d6716be8',
+    ),
+  ),
 );
