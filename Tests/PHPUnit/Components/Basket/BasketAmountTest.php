ecc22680ee256fba21eacb3030fe104af9a81858
Reinder van Bochove <work@iamreinder.dev>
2022-01-20 11:08:54 +0100

    Added testdox.

--- Tests/PHPUnit/Components/Basket/BasketAmountTest.php
+++ Tests/PHPUnit/Components/Basket/BasketAmountTest.php
@@ -7,7 +7,12 @@ use PHPUnit\Framework\TestCase;
 
 class BasketAmountTest extends TestCase
 {
-    public function testCanAddAmountToBasketAmount()
+    /**
+     * This test verifies that an amount can be set on the BasketAmount DTO.
+     *
+     * @testdox Method setAmount sets an amount on the BasketAmount DTO.
+     */
+    public function testCanSetAmountToBasketAmount()
     {
         $test = new \Enlight_Collection_ArrayCollection(['view' => '12345']);
 
@@ -19,7 +24,12 @@ class BasketAmountTest extends TestCase
         self::assertSame($expectedAmount, $basketAmount->getAmount());
     }
 
-    public function testCanAddCurrencyToBasketAmount()
+    /**
+     * This test verifies that a currency can be set on the BasketAmount DTO.
+     *
+     * @testdox Method setCurrency sets a currency on the BasketAmount DTO.
+     */
+    public function testCanSetCurrencyToBasketAmount()
     {
         $expectedCurrency = 'EUR';
 
@@ -29,7 +39,13 @@ class BasketAmountTest extends TestCase
         self::assertSame($expectedCurrency, $basketAmount->getCurrency());
     }
 
-    public function testCanAddAmountAndCurrencyInConstructor()
+    /**
+     * This test verifies that an amount and a currency can be set on the BasketAmount DTO
+     * through the constructor.
+     *
+     * @testdox Constructor sets an amount and a currency on the BasketAmount DTO.
+     */
+    public function testCanSetAmountAndCurrencyInConstructor()
     {
         $expectedAmount = 25.5;
         $expectedCurrency = 'USD';
