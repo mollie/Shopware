1e4a4bfd7bc998dbb92fa7fddce0dc878ecdcdc8
Reinder van Bochove <42994209+iamreinder@users.noreply.github.com>
2022-05-23 08:53:06 +0200

    MOL-755: Added confirmation email sent flag and tests. (#325)

--- Facades/FinishCheckout/Services/ConfirmationMail.php
+++ Facades/FinishCheckout/Services/ConfirmationMail.php
@@ -2,41 +2,48 @@
 
 namespace MollieShopware\Facades\FinishCheckout\Services;
 
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
 use MollieShopware\Models\Transaction;
 use MollieShopware\Models\TransactionRepository;
 
 class ConfirmationMail
 {
-
     /**
-     * @var $sOder
+     * @var $sOrder
      */
-    private $sOder;
+    private $sOrder;
 
     /**
      * @var TransactionRepository
      */
     private $repoTransaction;
 
-
     /**
-     * ConfirmationMail constructor.
-     * @param $sOder
+     * Creates a new instance of the confirmation mail service.
+     *
+     * @param $sOrder
      * @param TransactionRepository $repoTransaction
      */
-    public function __construct($sOder, TransactionRepository $repoTransaction)
+    public function __construct($sOrder, TransactionRepository $repoTransaction)
     {
-        $this->sOder = $sOder;
+        $this->sOrder = $sOrder;
         $this->repoTransaction = $repoTransaction;
     }
 
-
     /**
+     * Sends a confirmation email for the provided transaction object.
+     *
      * @param Transaction $transaction
+     *
      * @throws \Exception
      */
     public function sendConfirmationEmail(Transaction $transaction)
     {
+        if ($transaction->getConfirmationMailSent()) {
+            throw new \Exception('The confirmation e-mail is already sent.');
+        }
+
         $variables = @json_decode($transaction->getOrdermailVariables(), true);
 
         if (!is_array($variables)) {
@@ -46,19 +53,38 @@ class ConfirmationMail
             throw new \Exception('Required OrderMailVariables are NULL. Confirmation mail cannot be sent without data! JSON Decode Error Code: ' . $errorCode);
         }
 
+        $this->sOrder->sUserData = $variables;
 
-        $this->sOder->sUserData = $variables;
-
-        if (!is_array($this->sOder->sBasketData)) {
-            $this->sOder->sBasketData = [];
+        if (!is_array($this->sOrder->sBasketData)) {
+            $this->sOrder->sBasketData = [];
         }
-        $this->sOder->sBasketData['sCurrencyName'] = $transaction->getCurrency();
+
+        $this->sOrder->sBasketData['sCurrencyName'] = $transaction->getCurrency();
 
         if (isset($variables['additional']['charge_vat']) && $variables['additional']['charge_vat'] === false) {
-            $this->sOder->sNet = true;
+            $this->sOrder->sNet = true;
         }
 
-        $this->sOder->sendMail($variables);
+        $this->sOrder->sendMail($variables);
+
+        $this->setConfirmationMailSentFlag($transaction);
     }
 
+    /**
+     * Stores a confirmation mail sent flag
+     * on the provided transaction entity.
+     *
+     * @param Transaction $transaction
+     *
+     * @return void
+     *
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    private function setConfirmationMailSentFlag(Transaction $transaction)
+    {
+        $transaction->setConfirmationMailSent(true);
+
+        $this->repoTransaction->save($transaction);
+    }
 }
