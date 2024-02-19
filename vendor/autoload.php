932c607f7a492236de0b5ac55343fc642bc4210c
Vitalij Mik <vitalij.mik@dasistweb.de>
2024-02-19 14:46:35 +0100

    NTR: prepare hotfix

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitaaf5be21a6926c7c633053907bbe57b7::getLoader();
+return ComposerAutoloaderInit048a628bc63e0bdd9ef175499008e74a::getLoader();
